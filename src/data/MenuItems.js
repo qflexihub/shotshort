565f81ca558e8b8f09e9d29f35b9a3fe4195e57b
rajat <rajat22999@gmail.com>
2023-05-07 18:34:05 +0530

    Worked on Navbar menu

--- src/data/MenuItems.js
+++ src/data/MenuItems.js
@@ -1,19 +1,41 @@
 export const MenuItems = [
-    {
-      title: "Services",
-      link: "/services",
-    },
-    {
-      title: "Blogs",
-      link: "/blogs",
-    },
-    {
-      title: "About Us",
-      link: "/about-us",
-    },
-    {
-      title: "Case Studies",
-      link: "/case-studies",
-    },
-  ];
-  
+  {
+    title: "Services",
+    link: "/services",
+  },
+  {
+    title: "Blogs",
+    link: "/blogs",
+  },
+  {
+    title: "About Us",
+    link: "/about-us",
+  },
+  {
+    title: "Case Studies",
+    link: "/case-studies",
+  },
+];
+
+export const dropdownItems = [
+  {
+    title: "Social Media Videos",
+    link: "/social-media-video",
+  },
+  {
+    title: "Video Ads",
+    link: "/video-ads",
+  },
+  {
+    title: "Product Video",
+    link: "/product-video",
+  },
+  {
+    title: "Facebook Ads",
+    link: "/facebook-ads",
+  },
+  {
+    title: "Influencer Marketing",
+    link: "/influencer-marketing",
+  },
+];
