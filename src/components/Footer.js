904a486652130c93d545cd4c6bd6dc5de6f1f2c6
ankush <qflexihub@gmail.com>
2023-05-07 14:52:48 +0530

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,77 @@
+import Image from "next/image";
 import styled from "styled-components";
+import { SocialMediaHandles } from './../data/SocialMediaHandles';
 
 const Footer = () => {
   return (
-    <>
-      <FooterContainer>Footer</FooterContainer>
-    </>
+    <FooterContainer>
+      <BrandInfo>
+        <Image
+          width={100}
+          height={75}
+          src="/logo.png"
+          alt=""
+        />
+        <SocialMediaIcons>
+          {SocialMediaHandles?.map((item, index) => {
+            {console.log(index, item.title)}
+            <Image
+              key={item.id}
+              width={25}
+              height={25}
+              src={item.icon}
+              alt={item.title}
+            />
+          })}
+        </SocialMediaIcons>
+        <FindUs>
+          Finds us online
+        </FindUs>
+      </BrandInfo>
+      <ContactInfo>
+        Contact Info
+      </ContactInfo>
+      <ServicesInfo>
+        Services Info
+      </ServicesInfo>
+    </FooterContainer>
   );
 };
 export default Footer;
 
-const FooterContainer = styled.div``;
+const FooterContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-evenly;
+  max-width: 100%;
+  padding: 50px 120px;
+  background: #24093F;
+`;
+
+const BrandInfo = styled.div`
+  height: 250px;
+  flex: 3;
+`;
+
+const FindUs = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+  text-transform: capitalize;
+  color: rgba(255, 255, 255, 0.7);
+  padding: 25px 0;
+`;
+
+const SocialMediaIcons = styled.div`
+  background: red;
+`;
+const ContactInfo = styled.div`
+  height: 250px;
+  flex: 4;
+`;
+
+const ServicesInfo = styled.div`
+  height: 250px;
+  flex: 5;
+`;
---
+++ src/data/SocialMediaHandles.js
@@ -0,0 +1,26 @@
+export const SocialMediaHandles = [
+  {
+    id: 1,
+    title: "Facebook",
+    url:"www.facebook.com/user",
+    icon: "icons/facebook.svg",
+  },
+  {
+    id: 2,
+    title: "Instagram",
+    url:"www.instagram.com/user",
+    icon: "icons/instagram.svg",
+  },
+  {
+    id: 3,
+    title: "Youtube",
+    url:"www.youtube.com/user",
+    icon: "icons/youtube.svg",
+  },
+  {
+    id: 4,
+    title: "Twitter",
+    url:"www.twitter.com/user",
+    icon: "icons/twitter.svg",
+  },
+];
