ba71d1192c822e6bf93ebdf3eed06b9cbbf85f20
rajat <rajat22999@gmail.com>
2023-05-07 13:46:41 +0530

    Worked on Video Component and swiper

--- src/components/BrandVideos.js
+++ src/components/BrandVideos.js
@@ -1,12 +1,24 @@
 import styled from "styled-components";
 import VideoCard from "./shared/videoCard";
-import { Fragment } from "react";
+import { Fragment, useRef } from "react";
 import { Videos } from "@/data/BrandVideosData";
 import Heading from "./shared/Heading";
 import Text from "./shared/Text";
 import Button from "./shared/Button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+// import { Navigation } from "swiper";
+import SwiperCore, { Navigation } from "swiper/core";
+
+import Image from "next/image";
+
+SwiperCore.use([Navigation]);
 
 const BrandVideos = () => {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
   return (
     <>
       <BrandVideoContainer>
@@ -26,13 +38,51 @@ const BrandVideos = () => {
           />
         </Block1>
         <VideoContainer>
-          {Videos?.map((item, index) => {
-            return (
-              <Fragment key={index}>
-                <VideoCard data={item} />
-              </Fragment>
-            );
-          })}
+          <Swiper
+            slidesPerView={4}
+            slidesSkip={1}
+            spaceBetween={10}
+            // navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
+            navigation={{
+              prevEl: ".swiper-button-prev",
+              nextEl: ".swiper-button-next",
+            }}
+            // breakpoints={{
+            //   769: {
+            //     slidesPerView: 2,
+            //     slidesPerGroup: 2,
+            //   },
+            // }}
+            className="mySwiper"
+          >
+            <>
+              {Videos?.map((item, index) => {
+                return (
+                  <>
+                    <SwiperSlide key={index}>
+                      <VideoCard data={item} />
+                    </SwiperSlide>
+                  </>
+                );
+              })}
+            </>
+          </Swiper>
+          <NextArrow className="swiper-button-next" ref={nextRef}>
+            <Image
+              width={15}
+              height={15}
+              src="/next-arrow.svg"
+              alt="Prev Arrow"
+            />
+          </NextArrow>
+          <PrevArrow className="swiper-button-prev" ref={prevRef}>
+            <Image
+              width={15}
+              height={15}
+              src="/prev-arrow.svg"
+              alt="Next Arrow"
+            />
+          </PrevArrow>
         </VideoContainer>
       </BrandVideoContainer>
     </>
@@ -52,6 +102,41 @@ const Block1 = styled.div`
 const VideoContainer = styled.div`
   display: flex;
   justify-content: center;
-  gap: 20px;
+  gap: 10px;
   margin-top: 40px;
+  position: relative;
+`;
+
+const NextArrow = styled.div`
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  background-color: #8218ea;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+  right: -20px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+  border: 3px solid #fff;
+`;
+
+const PrevArrow = styled.div`
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  background-color: #8218ea;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+  left: 0;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+  border: 3px solid #fff;
 `;
--- src/data/BrandVideosData.js
+++ src/data/BrandVideosData.js
@@ -1,22 +1,34 @@
 export const Videos = [
   {
-    url: "/video1.mp4",
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/0bb5bb9a-577f-46a5-a7bc-ed3eef650e72/MP4/FabAlley.mp4",
     brandLogo: "/brand-logo1.svg",
   },
   {
-    url: "/video2.mp4",
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/8dbefc6f-2268-4c59-a3f0-efe07f04f744/MP4/jewel-kart.mp4",
     brandLogo: "/brand-logo2.svg",
   },
   {
-    url: "/video3.mp4",
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/3e8b35f6-061e-41ac-a476-c567180b17b8/MP4/HnM.mp4",
     brandLogo: "/brand-logo3.svg",
   },
   {
-    url: "/video4.mp4",
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/cf10ebd3-01ae-499b-9a15-0adcb589507b/MP4/lorealparis.mp4",
     brandLogo: "/brand-logo1.svg",
   },
-  // {
-  //   url: "/video5.mp4",
-  //   brandLogo: "/brand-logo2.svg",
-  // },
+  {
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/7551a597-ed02-417b-9ebc-659ff6dc0c8e/MP4/DermacoIndia.mp4",
+    brandLogo: "/brand-logo2.svg",
+  },
+  {
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/50e34092-ddba-486b-9e5a-9b2875438dc7/MP4/thebeautystorebybigbasket.mp4",
+    brandLogo: "/brand-logo1.svg",
+  },
+  {
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/a11a002b-8144-46ad-b75e-81c53c5612e4/MP4/Avvatar.mp4",
+    brandLogo: "/brand-logo2.svg",
+  },
+  {
+    url: "https://drl5d07sf56ow.cloudfront.net/assets/top6/ab881645-53ac-452a-a631-7426d33e3204/MP4/Onlyearth.mp4",
+    brandLogo: "/brand-logo3.svg",
+  },
 ];
